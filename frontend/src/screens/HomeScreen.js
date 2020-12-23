5f5b522dc595062103f923df5b6e16e1290c19f3
Kit Fuderich <kitfuderich@gmail.com>
2020-12-23 15:13:40 -0800

    redux store implemented w/chrome dev tools for debugging

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import data from '../data';
+import React, { useEffect } from 'react'
+//import data from '../data';
 import Product from '../components/Product'
-import axios from 'axios';
+
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 export default function HomeScreen() {
-
-  const [products,setProducts] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-
-  const [error,setError] = useState(false);
-
+const dispatch = useDispatch();
+const productList = useSelector((state)=>state.productList);
+const {loading,error,products} = productList;
    
   useEffect(()=>{
-    setLoading(true);
-    const fetchData = async () =>{
-      try{
-        const {data} = await axios.get('/api/products');
-        setLoading(false);
-        setProducts(data);
-      }
-      catch(err){
-        setError(err.message)
-        setLoading(false);
-      }
-   
-    };
-    fetchData();
-   },[])
+   dispatch(listProducts());
+   },[dispatch])
 
 
 
@@ -42,7 +27,7 @@ export default function HomeScreen() {
               ):(
         <div className="row center">
           {
-            data.products.map(product =>(
+            products.map(product =>(
           <Product key={product._id} product ={product}></Product>
             ))}
         </div>
