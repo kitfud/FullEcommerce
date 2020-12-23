e1f48c2fea2d5de92034ff3396c2f5b3adaa9617
Kit Fuderich <kitfuderich@gmail.com>
2020-12-23 08:50:51 -0800

    react app created and static html converted into first page of app

---
+++ frontend/src/App.js
@@ -0,0 +1,212 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div class="grid-container">
+    <header class="row">
+        <div>
+            <a class ="brand" href="index.html">GardenSurplus</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+
+    <main >
+       <div class="row center">
+         <div class="card">
+             <a href="product.html">
+                
+                 <img class="medium" src="./images/p1.jpg" alt="product"/>
+             </a>
+             <div class="card-body">
+                <a href="product.html">
+                    <h2>Shiitake Agar Plate</h2>
+                </a>
+                <div class="rating">
+                    <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                    <span>
+                        <i class="fa fa-star"></i>
+                    </span>
+                </div>
+                <div class="price">
+                    $8
+                </div>
+             </div>
+         </div>
+         <div class="card">
+            <a href="product.html">
+              
+                <img class="medium" src="./images/p1.jpg" alt="product"/>
+            </a>
+            <div class="card-body">
+               <a href="product.html">
+                   <h2>Shiitake Agar Plate</h2>
+               </a>
+               <div class="rating">
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+               </div>
+               <div class="price">
+                   $8
+               </div>
+            </div>
+        </div>
+        <div class="card">
+            <a href="product.html">
+               
+                <img class="medium" src="./images/p1.jpg" alt="product"/>
+            </a>
+            <div class="card-body">
+               <a href="product.html">
+                   <h2>Shiitake Agar Plate</h2>
+               </a>
+               <div class="rating">
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+               </div>
+               <div class="price">
+                   $8
+               </div>
+            </div>
+        </div>
+        <div class="card">
+            <a href="product.html">
+              
+                <img class="medium" src="./images/p1.jpg" alt="product"/>
+            </a>
+            <div class="card-body">
+               <a href="product.html">
+                   <h2>Shiitake Agar Plate</h2>
+               </a>
+               <div class="rating">
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+               </div>
+               <div class="price">
+                   $8
+               </div>
+            </div>
+        </div>
+        <div class="card">
+            <a href="product.html">
+                
+                <img class="medium" src="./images/p1.jpg" alt="product"/>
+            </a>
+            <div class="card-body">
+               <a href="product.html">
+                   <h2>Shiitake Agar Plate</h2>
+               </a>
+               <div class="rating">
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fa fa-star"></i>
+                   </span>
+               </div>
+               <div class="price">
+                   $8
+               </div>
+            </div>
+        </div>
+        <div class="card">
+            <a href="product.html">
+               
+                <img class="medium" src="./images/p1.jpg" alt="product"/>
+            </a>
+            <div class="card-body">
+               <a href="product.html">
+                   <h2>Shiitake Agar Plate</h2>
+               </a>
+               <div class="rating">
+                   <span>
+                       <i class="fas fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fas fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fas fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="fas fa-star"></i>
+                   </span>
+                   <span>
+                       <i class="far fa-star"></i>
+                   </span>
+               </div>
+               <div class="price">
+                   $8
+               </div>
+            </div>
+        </div>
+       </div>
+    </main>
+
+    <footer class="row center">All Rights Reserved</footer>
+</div>
+  );
+}
+
+export default App;
