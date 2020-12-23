378709de1eede9adadac4d5a0fc4e732b2fffbea
Kit Fuderich <kitfuderich@gmail.com>
2020-12-23 09:36:07 -0800

    data array created for testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import data from './data';
 
 function App() {
+
+
   return (
-    <div class="grid-container">
-    <header class="row">
+    <div className="grid-container">
+    <header className="row">
         <div>
-            <a class ="brand" href="index.html">GardenSurplus</a>
+            <a className ="brand" href="index.html">GardenSurplus</a>
         </div>
         <div>
             <a href="cart.html">Cart</a>
@@ -14,197 +17,51 @@ function App() {
     </header>
 
     <main >
-       <div class="row center">
-         <div class="card">
-             <a href="product.html">
-                
-                 <img class="medium" src="./images/p1.jpg" alt="product"/>
-             </a>
-             <div class="card-body">
-                <a href="product.html">
-                    <h2>Shiitake Agar Plate</h2>
-                </a>
-                <div class="rating">
-                    <span>
-                        <i class="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i class="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i class="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i class="fa fa-star"></i>
-                    </span>
-                    <span>
-                        <i class="fa fa-star"></i>
-                    </span>
-                </div>
-                <div class="price">
-                    $8
-                </div>
-             </div>
-         </div>
-         <div class="card">
-            <a href="product.html">
-              
-                <img class="medium" src="./images/p1.jpg" alt="product"/>
-            </a>
-            <div class="card-body">
-               <a href="product.html">
-                   <h2>Shiitake Agar Plate</h2>
-               </a>
-               <div class="rating">
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-               </div>
-               <div class="price">
-                   $8
-               </div>
-            </div>
-        </div>
-        <div class="card">
-            <a href="product.html">
+       <div className="row center">
+         {
+           data.products.map(product =>(
+            <div key={product._id} className="card">
+            <a href={`/product/${product._id}`}>
                
-                <img class="medium" src="./images/p1.jpg" alt="product"/>
+                <img className="medium" src={product.image} alt={product.name}/>
             </a>
-            <div class="card-body">
-               <a href="product.html">
-                   <h2>Shiitake Agar Plate</h2>
+            <div className="card-body">
+               <a href={`/product/${product._id}`}>
+                   <h2>{product.name}</h2>
                </a>
-               <div class="rating">
+               <div className="rating">
                    <span>
-                       <i class="fa fa-star"></i>
+                       <i className="fa fa-star"></i>
                    </span>
                    <span>
-                       <i class="fa fa-star"></i>
+                       <i className="fa fa-star"></i>
                    </span>
                    <span>
-                       <i class="fa fa-star"></i>
+                       <i className="fa fa-star"></i>
                    </span>
                    <span>
-                       <i class="fa fa-star"></i>
+                       <i className="fa fa-star"></i>
                    </span>
                    <span>
-                       <i class="fa fa-star"></i>
+                       <i className="fa fa-star"></i>
                    </span>
                </div>
-               <div class="price">
-                   $8
+               <div className="price">
+                   ${product.price}
                </div>
             </div>
         </div>
-        <div class="card">
-            <a href="product.html">
-              
-                <img class="medium" src="./images/p1.jpg" alt="product"/>
-            </a>
-            <div class="card-body">
-               <a href="product.html">
-                   <h2>Shiitake Agar Plate</h2>
-               </a>
-               <div class="rating">
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-               </div>
-               <div class="price">
-                   $8
-               </div>
-            </div>
-        </div>
-        <div class="card">
-            <a href="product.html">
-                
-                <img class="medium" src="./images/p1.jpg" alt="product"/>
-            </a>
-            <div class="card-body">
-               <a href="product.html">
-                   <h2>Shiitake Agar Plate</h2>
-               </a>
-               <div class="rating">
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fa fa-star"></i>
-                   </span>
-               </div>
-               <div class="price">
-                   $8
-               </div>
-            </div>
-        </div>
-        <div class="card">
-            <a href="product.html">
-               
-                <img class="medium" src="./images/p1.jpg" alt="product"/>
-            </a>
-            <div class="card-body">
-               <a href="product.html">
-                   <h2>Shiitake Agar Plate</h2>
-               </a>
-               <div class="rating">
-                   <span>
-                       <i class="fas fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fas fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fas fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="fas fa-star"></i>
-                   </span>
-                   <span>
-                       <i class="far fa-star"></i>
-                   </span>
-               </div>
-               <div class="price">
-                   $8
-               </div>
-            </div>
-        </div>
+
+
+
+           ))
+         }
+    
+      
        </div>
     </main>
 
-    <footer class="row center">All Rights Reserved</footer>
+    <footer className="row center">All Rights Reserved</footer>
 </div>
   );
 }
