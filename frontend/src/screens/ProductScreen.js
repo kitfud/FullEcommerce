67c4e9044a85ea8fb258dd8e339d891a4fe04a21
Kit Fuderich <kitfuderich@gmail.com>
2020-12-24 09:55:18 -0800

    remove from cart implemented

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -26,37 +26,38 @@ dispatch(detailsProduct(productId));
 
     return (
         <div>
+
         {loading ? (
         <LoadingBox></LoadingBox>
-          ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
+        ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
             ):(
                 <div>
                 <Link to="/">Back To Home</Link>
-          <div className="row top">
-              <div className="col-2">
-                 <img className = "large" src={product.image} alt={product.name}/>
-              </div>
-              <div className="col-1">
-         <ul>
-             <li>
-                 <h1>{product.name}</h1>
-             </li>
-             <li>
-                 <Rating rating = {product.rating} numReviews= {product.numReviews}></Rating>
-             </li>
-             <li>Price: ${product.price}</li>
-             <li>Description:
-                 <p className = "description">{product.description}</p>
-             </li>
-         </ul>
-              </div>
-              <div className="col-1">
-             <div className = "card card-body">
-             <ul>
-                 <li>
-                     <div className="row">
-                         <div>Price</div>
+        <div className="row top">
+        <div className="col-2">
+                <img className = "large" src={product.image} alt={product.name}/>
+            </div>
+            <div className="col-1">
+        <ul>
+        <li>
+                <h1>{product.name}</h1>
+            </li>
+            <li>
+                <Rating rating = {product.rating} numReviews= {product.numReviews}></Rating>
+            </li>
+            <li>Price: ${product.price}</li>
+            <li>Description:
+                <p className = "description">{product.description}</p>
+            </li>
+        </ul>
+            </div>
+            <div className="col-1">
+            <div className = "card card-body">
+            <ul>
+            <li>
+                    <div className="row">
+                        <div>Price</div>
                          <div className = "price">
                              ${product.price}
                          </div>
@@ -111,6 +112,7 @@ dispatch(detailsProduct(productId));
           </div>
              </div>
       )}
-      </div> 
+
+    </div> 
     )
 }
