f5de01e197ee769bb3a347c53bf0785637a91431
Kit Fuderich <kitfuderich@gmail.com>
2020-12-24 12:21:19 -0800

    sign in route created as back end api using JWT + .env file created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv'
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
+dotenv.config();
+
 const app = express();
+//parsing data in body of request
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/gardensurplus',
 {
   useNewUrlParser: true,
