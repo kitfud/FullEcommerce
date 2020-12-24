59573e25b60f9a8520e02e4d4037a55c898052ef
Kit Fuderich <kitfuderich@gmail.com>
2020-12-24 11:00:26 -0800

    mongoDB database setup and migrated a few users with seed function

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users:[
+        {
+            name:'Kit',
+            email:'kitfuderich@gmail.com',
+            password: bcrypt.hashSync('1234',8),
+            isAdmin:true,
+        },
+        {
+            name:'John',
+            email:'example@example.com',
+            password: bcrypt.hashSync('1234',8),
+            isAdmin:false,
+        },
+
+    ],
     products:[
         {
             _id:'1',
