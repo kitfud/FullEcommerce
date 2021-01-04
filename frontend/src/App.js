4636ad57ce8c68445affbcc5d9aaf175c2980f77
Kit Fuderich <kitfuderich@gmail.com>
2021-01-04 10:30:01 -0800

    payment checkout screen created w/ styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import SigninScreen from './screens/SigninScreen';
 import { signout } from './actions/userActions';
 import RegisterScreen from './screens/ RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
 
@@ -62,6 +63,7 @@ function App() {
       <Route path = "/product/:id" component= {ProductScreen}></Route>
       <Route path = "/signin" component = {SigninScreen}></Route>
       <Route path = "/register" component = {RegisterScreen}></Route>
+      <Route path="/shipping" component = {ShippingAddressScreen}></Route>
       <Route path="/" component={HomeScreen} exact></Route>
     </main>
 
